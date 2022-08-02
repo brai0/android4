c56a86d202ae13453eab250a503301c3ac0ac11b
brai0 <98900545+brai0@users.noreply.github.com>
2022-08-02 12:03:08 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,40 +1,62 @@
-package com.example.fourtha;
+package com.example.fourthb;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity
+{
+    private static final int REQUEST_CODE_SELECT_IMAGE = 2;
     private final int CAMERA_REQ_CODE=100;
-    ImageView imgC;
+    ImageView imageSelected;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imgC=findViewById(R.id.imgCamera);
-        Button cameraBtn=findViewById(R.id.btnCamera);
+        imageSelected = findViewById(R.id.imageView);
 
-        cameraBtn.setOnClickListener(view -> {
+        findViewById(R.id.buttonSelectedImage).setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent,REQUEST_CODE_SELECT_IMAGE);
+        });
+        findViewById(R.id.buttonOpenCamera).setOnClickListener(view -> {
             Intent iCamera =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             startActivityForResult(iCamera,CAMERA_REQ_CODE);
         });
+    }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data != null){
+            if(requestCode==REQUEST_CODE_SELECT_IMAGE){
+            Uri selectedImageUri = data.getData();
+            InputStream inputStream = null;
+            try {
+                inputStream = getContentResolver().openInputStream(selectedImageUri);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            imageSelected.setImageBitmap(bitmap);
         }
-    protected void onActivityResult(int  requestCode,int resultCode,@Nullable Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        if(resultCode==RESULT_OK){
-            if(requestCode==CAMERA_REQ_CODE){
+        else if (requestCode==CAMERA_REQ_CODE)
+            {
                 assert data != null;
                 Bitmap img= (Bitmap) data.getExtras().get("data");
-                imgC.setImageBitmap(img);
+                imageSelected.setImageBitmap(img);
             }
         }
-    }}
-
+    }
+}
